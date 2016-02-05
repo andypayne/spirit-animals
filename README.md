6a2180cc215b9429a35ed7ab0a62457182602fc0
Aaron McCall <aaron@andyet.net>
2016-02-05 12:27:15 -0700

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # spirit-animals
 A helper to generate spirit animal names.
+
+## usage
+
+```javascript
+var spiritAnimals = require('spirit-animals');
+
+var one = spiritAnimals.one();
+// one is 
+'LovingIndigoWhitetail'
+
+var five = spiritAnimals.get(5);
+// five is
+[
+  'ContemplativeTealBeetle',
+  'UnwaveringWhiteRidley',
+  'SensibleFuchsiaMockingbird',
+  'SteadyPlumBoa',
+  'ConvivialPurpleCottontail'
+]
+```
---
+++ index.js
@@ -0,0 +1,46 @@
+var sample = require('lodash.sample');
+var sampleSize = require('lodash.sampleSize');
+var zip = require('lodash.zip');
+
+var adjectives = require('./adjectives');
+var animals = require('./animals');
+var colors = require('./colors');
+
+function getSampleOf(array, size) {
+  if (size > array.length) {
+    var len = array.length;
+    var remaining = size;
+    var out = [];
+    while (remaining > len) {
+      out.push.apply(out, sampleSize(array, len));
+      remaining -= len;
+    }
+    if (remaining > 0) {
+      out.push.apply(out, sampleSize(array, remaining));
+    }
+    return out;
+  } else {
+    return sampleSize(array, size);
+  }
+}
+
+module.exports = {
+  one: function () {
+    return sample(adjectives) + sample(colors) + sample(animals);
+  },
+  get: function (num) {
+    var number = parseInt(num, 10);
+    if (isNaN(number)) {
+      throw TypeError('spirit-animals#get must be called with a number, not ' + num);
+    }
+    var adj, col, anim;
+
+    return zip(
+      getSampleOf(adjectives, number),
+      getSampleOf(colors, number),
+      getSampleOf(animals, number)
+    ).map(function (parts) {
+      return parts.join('');
+    });
+  }
+};
